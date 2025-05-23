3804d249a950fa3324024af1d4c1299f7fb920b0
AdrielFierro <adri.fierro@outlook.com>
2025-05-23 19:10:58 -0300

    Start of the proyect

---
+++ demo/src/main/java/com/example/demo/TpoDesap1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoDesap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpoDesap1Application.class, args);
+	}
+
+}
